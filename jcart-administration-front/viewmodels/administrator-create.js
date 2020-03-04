58f4dd98a0466d1d5320d6ba0a522913e1a1d4fb
mengqisong <1160510138@qq.com>
2020-03-04 16:25:01 +0800

    add administrator create uploadImage

--- jcart-administration-front/viewmodels/administrator-create.js
+++ jcart-administration-front/viewmodels/administrator-create.js
@@ -10,12 +10,41 @@ var app = new Vue({
         status:[
             { value: 1, label: '启用' },
             { value: 0, label: '禁用' }
-        ]
+        ],
+        mainFileList: [],
+        selectedMainPic: '',
+        mainPicUrl: ''
     },
     mounted(){
         console.log('view mounted');
     },
     methods: {
+        handleUploadMainClick(){
+            console.log('upload main pic click');
+            this.uploadMainImage();
+        },
+        handleOnMainChange(val) {
+            this.selectedMainPic = val.raw;
+        },
+        uploadMainImage() {
+            var formData = new FormData();
+            formData.append("image", this.selectedMainPic);
+
+            axios.post('/image/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    app.mainPicUrl = response.data;
+                    alert('上传成功');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    alert('上传失败');
+                });
+        },
         handleCreateClick(){
             console.log('create click');
             this.createAdministrator();
