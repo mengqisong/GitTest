843304c4846801989d38e4fdc3d6574a974e47d1
mengqisong <1160510138@qq.com>
2020-03-10 17:46:35 +0800

    add administrator product search front

--- jcart-administration-front/viewmodels/product-search.js
+++ jcart-administration-front/viewmodels/product-search.js
@@ -2,21 +2,49 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo:'',
-        pageNum: 1
+        pageNum: 1,
+        productCode: '',
+        productName: '',
+        price: '',
+        stockQuantity: '',
+        selectedStatus: '',
+        statuses: [
+            { value: 0, label: '下架' },
+            { value: 1, label: '上架' },
+            { value: 2, label: '待审核' }
+        ]
     },
     mounted(){
         console.log("view mounted");
         this.searchProduct();
     },
     methods:{
+        handleSearchClick(){
+            console.log('search click');
+            this.pageNum = 1;
+            this.searchProduct();
+        },
         handlePageChange(val){
             console.log('page change');
             this.pageNum = val;
             this.searchProduct();
         },
+        handleClearClick() {
+            console.log('clear click');
+            this.productCode = '';
+            this.productName = '';
+            this.price = '';
+            this.stockQuantity = '';
+            this.selectedStatus = '';
+        },
         searchProduct(){
             axios.get('/product/search', {
                 params: {
+                    productCode: this.productCode,
+                    productName: this.productName,
+                    price: this.price,
+                    stockQuantity: this.stockQuantity,
+                    status: this.selectedStatus,
                     pageNum: this.pageNum
                 }
             })
