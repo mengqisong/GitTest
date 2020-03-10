0d324d4ac5be0e675fcdc75fbe47e1c61fa68fb3
mengqisong <1160510138@qq.com>
2020-03-10 18:43:48 +0800

    add administrator return search completion

--- jcart-administration-back/src/main/java/io/mqs/jcartadministrationback/service/ReturnService.java
+++ jcart-administration-back/src/main/java/io/mqs/jcartadministrationback/service/ReturnService.java
@@ -1,12 +1,13 @@
 package io.mqs.jcartadministrationback.service;
 
 import com.github.pagehelper.Page;
+import io.mqs.jcartadministrationback.dto.in.ReturnSearchInDTO;
 import io.mqs.jcartadministrationback.po.Return;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReturnService {
-    Page<Return> search(Integer pageNum);
+    Page<Return> search(ReturnSearchInDTO returnSearchInDTO,Integer pageNum);
 
     Return getById(Integer returnId);
 
