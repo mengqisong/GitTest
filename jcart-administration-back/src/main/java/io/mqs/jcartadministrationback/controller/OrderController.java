49591ac358a3c6bab7d5e37af66e3f707434f4a9
mengqisong <1160510138@qq.com>
2020-03-10 21:01:29 +0800

    add administrator order search front completion

--- jcart-administration-back/src/main/java/io/mqs/jcartadministrationback/controller/OrderController.java
+++ jcart-administration-back/src/main/java/io/mqs/jcartadministrationback/controller/OrderController.java
@@ -5,15 +5,13 @@ import io.mqs.jcartadministrationback.dto.in.OrderSearchInDTO;
 import io.mqs.jcartadministrationback.dto.out.*;
 import io.mqs.jcartadministrationback.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/order")
+@CrossOrigin
 public class OrderController {
 
     @Autowired
