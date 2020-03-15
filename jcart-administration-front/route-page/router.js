38ca3b5c136783196f0d5a30728e45e06bd328f8
mengqisong <1160510138@qq.com>
2020-03-15 22:27:47 +0800

    fix navi

--- jcart-administration-front/route-page/router.js
+++ jcart-administration-front/route-page/router.js
@@ -1,6 +1,10 @@
 const routes = [
     { path: '/product/search', component: ProductSearchRoutePage },
     { path: '/customer/search', component: CustomerSearchRoutePage },
+    { path: '/order/search', component: OrderSearchRoutePage },
+    { path: '/return/search', component: ReturnSearchRoutePage },
+    
+    
 ];
 
 const router = new VueRouter({
